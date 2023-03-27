bcfcab4d1504fc4049f12d753145b732d960498c
minijus <3633549+minijus@users.noreply.github.com>
2023-03-27 08:38:44 +0300

    add validators and observe content input length

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,51 @@
-import {Component} from '@angular/core';
-import {FormBuilder} from "@angular/forms";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { map, Observable, startWith, Subject, takeUntil } from 'rxjs';
 
 const fb = new FormBuilder();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+  public contentMaxLength = 50;
+  public contentLengthRemaining$!: Observable<number>;
   postForm = fb.group({
-    title: [''],
-    content: ['']
+    title: ['', [Validators.required, Validators.minLength(5)]],
+    content: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(this.contentMaxLength),
+      ],
+    ],
   });
 
+  ngOnInit(): void {
+    this.contentLengthRemaining$ = this.postForm
+      .get('content')!
+      .valueChanges.pipe(
+        startWith(''),
+        takeUntil(this.destroy$),
+        map((value) => {
+          return this.contentMaxLength - (value?.length || 0);
+        }),
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+  }
+
   onPostFormSubmit() {
-    console.log(this.postForm.value)
-    this.postForm.reset();
+    if (this.postForm.valid) {
+      console.log(this.postForm.value);
+      this.postForm.reset();
+    }
   }
 
   onPostFormReset() {
